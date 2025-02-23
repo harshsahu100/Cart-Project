d82499b40abe07cde22a837c2ed3bc152f9314fe
harshsahu100 <sahuharsh097@gmail.com>
2025-02-23 12:42:49 +0530

    Create style.js
    first commit

---
+++ style.js
@@ -0,0 +1,8 @@
+function add(){
+    let value=document.getElementById("cart").value;
+    alert(value +" "+ "item added");
+}
+function reset(){
+    value=0;
+    alert(value+" "+ "item left");
+}
